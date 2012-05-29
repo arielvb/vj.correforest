e97d28d78c7585b30893b49e03a6cfda8fe515e3
arielvb <i@arielvb.com>
2012-05-29 21:08:18 +0200

    Sound support, device orientation support

--- correforest.js
+++ correforest.js
@@ -29,7 +29,7 @@ var CorreForest;
 		return 'Speed ' + (new Array(speed + 1)).join('♦');
 	}
 
-	function newRoad(l, r) {
+	function newRoad(l, r, max) {
 		var offset = Math.floor(Math.random() * 3);
 		if (offset === 0) {
 			offset = -1;
@@ -42,10 +42,17 @@ var CorreForest;
 		if (l + offset < 0) {
 			offset = 0;
 		}
-		if (r + offset > 490) {
+		if (r + offset > max) {
 			offset = 0;
 		}
 		return [l + offset, r + offset];
+	}
+
+	function createDivAndAppend(className, parent){
+		var l = document.createElement('div');
+		l.className = className;
+		parent.appendChild(l);
+		return l;
 	}
 
 	// The class for the Game
@@ -60,57 +67,53 @@ var CorreForest;
 	CorreForest.prototype = {
 		restart: false,
 		gameover: false,
+		moved: 2,
 		lives: 0,
 		play: true,
 		metros: 0,
 		position: {x: 0, y: 0},
 		init: function () {
-			var i;
+			var i, width, height, road = {};
 			// Read configuration
 			this.lives = this.config.lives;
 			this.speed = this.config.speed.min;
 			this.play = this.config.autoplay;
 			this.gameover = false;
 			this.metros = 0;
-			this.road = [];
-			this.position = this.config.position;
 			// Init the road
+			this.road = [];
+			height = this.config.height;
+			width = this.config.width;
+			road.left = (width - this.config.road) / 2;
+			road.right = (width + this.config.road) / 2
 			for (i = 0; i < this.config.position.y; i += 1) {
-				this.road.push([500 / 2 - this.config.road / 2, 500 / 2 + this.config.road / 2]);
+				this.road.push([road.left, road.right]);
 			}
-			for (i = this.config.position.y; i < 600 / 10; i += 1) {
-				this.road.push(newRoad(this.road[i - 1][0], this.road[i - 1][1]));
+			for (i = this.config.position.y; i < height / 10; i += 1) {
+				this.road.push(newRoad(this.road[i - 1][0], this.road[i - 1][1], width));
 			}
+			this.position = this.config.position;
+			if (this.position.x === 'middle'){
+				this.position.x = width / 20;
+			}
+			document.getElementsByTagName('audio')[0].volume = 0.5;
 		},
 		// Creates the UI (executed only once)
 		createUI: function (c) {
 			var i, wrapper, that = this;
 			wrapper = document.getElementById(c);
+			// Menu:  speed, length, lives
+			this.menubar = createDivAndAppend('menu', wrapper);
+			this.speedElement = createDivAndAppend('speed', this.menubar);
+			this.livesElement = createDivAndAppend('lives', this.menubar);
+			this.metrosElement = createDivAndAppend('counter', this.menubar)
 			this.canvas = document.createElement('canvas');
-			this.canvas.width = 500;
-			this.canvas.height = 600;
 			wrapper.appendChild(this.canvas);
-			// Menu: displays counters
-			this.menubar = document.createElement('div');
-			this.menubar.className = 'menu';
-			this.canvas.parentNode.insertBefore(this.menubar, this.canvas);
-			// speed counter
-			this.speedElement = document.createElement('div');
-			this.speedElement.className = 'speed';
-			this.menubar.appendChild(this.speedElement);
-			// Lives counter
-			this.livesElement = document.createElement('div');
-			this.livesElement.className = 'lives';
-			this.menubar.appendChild(this.livesElement);
-			// Length counter
-			this.metrosElement = document.createElement('div');
-			this.metrosElement.className = 'counter';
-			this.menubar.appendChild(this.metrosElement);
+			this.canvas.width = this.config.width;
+			this.canvas.height = this.config.height;
 			// Display: popup with a message
-			this.display = document.createElement('div');
-			this.display.className = 'display';
+			this.display = createDivAndAppend('display', wrapper);
 			this.display.style.display = 'none';
-			this.canvas.parentNode.insertBefore(this.display, this.canvas);
 			// Keyboard listener
 			// You can use the canvas as listener if you add the tab index property and will catch events
 			// wen the canvas is focused.
@@ -144,11 +147,54 @@ var CorreForest;
 					} else {
 						// Change play/pause
 						that.play = !that.play;
+						that.play ? document.getElementsByTagName('audio')[0].play() :  document.getElementsByTagName('audio')[0].pause();
+
 						// Display message on pause (!play)
 						message(that.display, !that.play, that.config.strings.pause);
 					}
 				}
 			}, true);
+			window.addEventListener("click", function (event){
+				if (event.clientX < that.canvas.offsetLeft + that.canvas.width/3){
+					event.preventDefault();
+					event.stopImmediatePropagation();
+        			event.stopPropagation();
+
+					that.position.x -= 1;
+				} else if (event.clientX > that.canvas.offsetLeft + that.canvas.width/3*2){
+					event.preventDefault();
+					that.position.x += 1;
+				} else{
+					if (that.gameover) {
+						event.preventDefault();
+						// Notify restart
+						that.restart = true;
+					} else {
+						event.preventDefault();
+						// Change play/pause
+						that.play = !that.play;
+						that.play ? document.getElementsByTagName('audio')[0].play() :  document.getElementsByTagName('audio')[0].pause();
+						// Display message on pause (!play)
+						message(that.display, !that.play, that.config.strings.pause);
+					}
+				}
+			}, false);
+			window.addEventListener("deviceorientation", function (event){
+				//console.log(event.gamma)
+				if (that.moved < 0){
+					if (event.gamma < -10){
+						event.preventDefault();
+						event.stopImmediatePropagation();
+	        			event.stopPropagation();
+						that.position.x -= 1;
+						that.moved = 1;
+					} else if (event.gamma > 10){
+						event.preventDefault();
+						that.moved = 1;
+						that.position.x += 1;
+					}
+				}
+			}, false);
 			// No autoplay => render background and display
 			if (!this.play) {
 				this.salida();
@@ -172,19 +218,22 @@ var CorreForest;
 			if (this.speed > this.config.speed.max) {
 				this.speed = this.config.speed.max;
 			} else if (this.speed < 1) {
-				this.speed = 1;
+				this.speed = this.config.speed.min;
 			}
 			colision = (this.road[this.position.y][0] >= this.position.x * 10) || (this.road[this.position.y][1] <= this.position.x * 10 + 5);
 			if (colision) {
 				this.lives -= 1;
 				this.speed = 1;
+				document.getElementsByTagName('audio')[1].currentTime = 0;
+				document.getElementsByTagName('audio')[1].play();
 				this.position.x = this.road[this.position.y][0] / 10 + 5;
 			} else {
 				this.metros += this.speed;
 			}
 			// New road's row
 			this.road.shift();
-			this.road.push(newRoad(this.road[this.road.length - 1][0], this.road[this.road.length - 1][1]));
+			this.road.push(newRoad(this.road[this.road.length - 1][0], this.road[this.road.length - 1][1], this.config.width));
+			this.moved -= 1;
 		},
 		// salida - draw the game and status
 		salida: function () {
@@ -203,9 +252,21 @@ var CorreForest;
 			// Translate player position
 			player.x = this.position.x * 10;
 			player.y = this.canvas.height - (this.position.y * 10);
-			context.fillRect(player.x, player.y, 10, 10);
+			// context.fillRect(player.x, player.y, 10, 10);
 			context.fillStyle  = this.config.colors.player.inside;
-			context.fillRect(player.x + 2, player.y + 2, 6, 6);
+			context.fillRect(player.x + 2, player.y, 1, 1);
+			context.fillRect(player.x + 7, player.y, 1, 1);
+			context.fillRect(player.x + 3, player.y + 1, 1, 1);
+			context.fillRect(player.x + 6, player.y + 1, 1, 1);
+			context.fillRect(player.x + 1, player.y + 2, 8, 1);
+			context.fillRect(player.x, player.y + 3, 4, 1);
+			context.fillRect(player.x + 5, player.y + 3, 1, 1);
+			context.fillRect(player.x + 7, player.y + 3, 3, 1);
+			context.fillRect(player.x, player.y + 4, 1, 3);
+			context.fillRect(player.x, player.y + 4, 10, 1);
+			context.fillRect(player.x + 9, player.y + 4, 1, 3);
+			context.fillRect(player.x + 2, player.y + 5, 6, 1);
+			context.fillRect(player.x + 3, player.y + 6, 4, 1);
 			context.stroke();
 			// Draw Speed, lives, and counter
 			this.speedElement.innerHTML = speed2string(this.speed);
@@ -217,6 +278,7 @@ var CorreForest;
 			var that = this;
 			if (this.restart) {
 				this.reset();
+				document.getElementsByTagName('audio')[0].play();
 				this.restart = false;
 			}
 			if (this.play) {
@@ -224,11 +286,12 @@ var CorreForest;
 				this.salida();
 			}
 			if (this.lives === 0) {
+				document.getElementsByTagName('audio')[0].pause();
 				this.gameover = true;
 				this.play = false;
 				message(this.display, true, this.config.strings.gameover);
 			}
-			setTimeout(function () { this.ciclo(); }.bind(this), 60 - this.speed * 5);
+			setTimeout(function () { that.ciclo(); }, 60 - this.speed * 3);
 		}
 	};
 }(window));
